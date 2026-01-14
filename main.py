9a69ac85018227100430d026d6192d1cce3340d1
Jasper De Gussemé <jasper.degusseme@ugent.be>
2026-01-14 22:20:21 +0100

    Enforced current working directory, added error-photo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import subprocess
 from flask import Flask, send_file
+import os
 
 
 app = Flask(__name__)
@@ -7,10 +8,13 @@ app = Flask(__name__)
 
 @app.route("/snapshot", methods=["GET"])
 def snapshot():
-    subprocess.run(["gphoto2", "--capture-image-and-download"])
-    result = send_file("capt0000.jpg", mimetype="image/jpg")
-    subprocess.run(["rm", "capt0000.jpg"])
-    return result
+    try:
+        subprocess.run(["gphoto2", "--capture-image-and-download"], cwd=os.getcwd())
+        result = send_file("capt0000.jpg", mimetype="image/jpg")
+        subprocess.run(["rm", "capt0000.jpg"], cwd=os.getcwd())
+        return result
+    except:
+        return send_file("backenderror.jpg", mimetype="image/jpg")
 
 
 if __name__ == "__main__":
